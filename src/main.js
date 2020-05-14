cde98ce27906cf7efc79eb69bc01bcb40abcd4ee
Fateful-am <fateful.am@gmail.com>
2020-05-14 20:45:15 +0300

    Добавит реализацую методов `getItems` и `setItem` в `store`

--- src/main.js
+++ src/main.js
@@ -9,8 +9,12 @@ const AUTHORIZATION = `Basic dXNickBwYXNzd75yZAo=`;
 // Точка доступа в сети
 const END_POINT = `https://11.ecmascript.pages.academy/cinemaddict/`;
 
+const STORE_PREFIX = `cinemaddict-localstorage`;
+const STORE_VER = `v1`;
+const STORE_NAME = `${STORE_PREFIX}-${STORE_VER}`;
+
 const api = new API(END_POINT, AUTHORIZATION);
-const store = new Store();
+const store = new Store(STORE_NAME, window.localStorage);
 const apiWithProvider = new Provider(api, store);
 
 const moviesModel = new Movies(apiWithProvider);
@@ -26,14 +30,14 @@ apiWithProvider.getMovies()
   })
   .catch(() => siteController.renderFilms());
 
-window.addEventListener(`load`, () => {
-  navigator.serviceWorker.register(`/sw.js`)
-    .then(() => {
-      // Действие, в случае успешной регистрации ServiceWorker
-    }).catch(() => {
-    // Действие, в случае ошибки при регистрации ServiceWorker
-    });
-});
+// window.addEventListener(`load`, () => {
+//   navigator.serviceWorker.register(`/sw.js`)
+//     .then(() => {
+//       // Действие, в случае успешной регистрации ServiceWorker
+//     }).catch(() => {
+//     // Действие, в случае ошибки при регистрации ServiceWorker
+//     });
+// });
 
 // отрисовка пустых фильмов
 moviesModel.setMovies([]);
